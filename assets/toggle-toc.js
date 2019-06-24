9c7ef77effe937a505c4b36062d2b004e6d63538
digikar99 <shubhamkarayare@gmail.com>
2019-06-24 21:43:48 +0530

    now page refresh not required for showing/hiding the toc-btn

--- assets/toggle-toc.js
+++ assets/toggle-toc.js
@@ -1,26 +1,32 @@
-TOCVisible = false;
-window.onload = function(e){
-    if (window.location.pathname == "/cl-cookbook/"){
-	document.getElementById("toc-btn").style.display = "none";
-    }else if ($(document).width() <= 576){
-	document.getElementById("toc-btn").style.display = "block";
-    }else{
-	document.getElementById("toc-btn").style.display = "none";
-    }
-}
+$(document).ready( function () {
+    TOCVisible = false;
 
-toggleTOC = function(){
-    toc = document.getElementById("toc-container");
-    toc_title = document.getElementById("toc-title");
-    if (!TOCVisible){
-	toc.classList.remove("toc-close");
-	toc.classList.add("toc-open");
-	TOCVisible = true;
+    function toggleTOCButton(e){
+	if (window.location.pathname == "/cl-cookbook/"){
+	    document.getElementById("toc-btn").style.display = "none";
+	}else if ($(document).width() <= 576){
+	    document.getElementById("toc-btn").style.display = "block";
+	}else{
+	    document.getElementById("toc-btn").style.display = "none";
+	}
     }
-    else{
-	toc.classList.remove("toc-open");
-	toc.classList.add("toc-close");
-	TOCVisible = false;
-    }
-}
 
+    setInterval(toggleTOCButton, 200);
+
+    toggleTOC = function(){
+	toc = document.getElementById("toc-container");
+	toc_title = document.getElementById("toc-title");
+	if (!TOCVisible){
+	    toc.classList.remove("toc-close");
+	    toc.classList.add("toc-open");
+	    TOCVisible = true;
+	}
+	else{
+	    toc.classList.remove("toc-open");
+	    toc.classList.add("toc-close");
+	    TOCVisible = false;
+	}
+    }
+
+    toggleTOCButton();
+})
