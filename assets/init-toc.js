5d54c1ce92552f9c06ef8f9316017eaa792726ed
vindarel <vincezd@gmail.com>
2025-09-16 15:24:37 +0200

    (minor) style

--- assets/init-toc.js
+++ assets/init-toc.js
@@ -2,8 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
   var $ = window.jQuery;
   var base =
     (document.documentElement.getAttribute("data-baseurl") || "") + "/";
+
+  // don't show the TOC on the index page.
   var onIndexPage = window.location.pathname === base;
 
+  // for safety, check jQuery is loaded and #toc exists.
   if (!onIndexPage && $ && $.fn && $.fn.toc && document.getElementById("toc")) {
     $("#toc").toc({
     content: "#content", // will ignore the first h1 with the site+page title.
